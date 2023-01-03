626b68516793b64291a98e370f191dd643f47bde
Ana Julia <anajuliasantiaggo@gmail.com>
2023-01-03 16:32:11 -0300

    create: Entity, Controller, Service, Repository and WebConfigure

---
+++ src/test/java/br/com/uniamerica/trasnportadora/transportadoraapi/TransportadoraApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uniamerica.trasnportadora.transportadoraapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportadoraApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
